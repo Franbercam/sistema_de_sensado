fe1b4845d633bd4cd2d1f0299605bde51f32000b
F. Bermúdez <franbercam01@gmail.com>
2024-04-09 21:08:15 +0200

    bug [broken pipe]

--- src/routes/ControlPanel.py
+++ src/routes/ControlPanel.py
@@ -1,10 +1,16 @@
 from flask import Blueprint
-from ..utils import TCP_connection
+from ..utils import TCP_connection as rb1
 
 main = Blueprint('index_blueprint', __name__)
 
 @main.route('/')
 
+#server_host = '192.168.8.109' 
+#server_port = 8888
+
 def index():
 
-    return tcp_prueba()
+    valor = rb1.conectar_servidor('192.168.8.109',8888)
+    return valor
+    #rb1.prueba()
+    #rb1.conectar_servidor('192.168.8.109',8888)
--- src/utils/TCP_connection.py
+++ src/utils/TCP_connection.py
@@ -1,33 +1,52 @@
 import socket
 
-"""
-#Dirección y puerto del servidor
-server_host = '169.254.249.146' 
-server_port = 8888
+def conectar_servidor(server_host, server_port):
+    # Socket para la conexión TCP/IP
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#Socket para la conexión TCP/IP
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-try:
-    client_socket.connect((server_host, server_port))
-    print("[*] Conexión exitosa al servidor")
-except Exception as e:
-    print(f"Error al conectar al servidor: {e}")
-    exit()
-
-while True:
     try:
-        data = client_socket.recv(1024) 
-        if not data:
-            break
-        print("Datos del sensor "+server_host+":" , data.decode('UTF-8'))
+        client_socket.connect((server_host, server_port))
+        try:
+            data = client_socket.recv(1024) 
+            if not data:
+                return ("No existe lectura del sensor")
+            else:
+                datos = ("Datos del sensor "+server_host+": " + data.decode('UTF-8'))
+                #client_socket.close()
+                return datos
+        except Exception as e:
+            return (f"Error al recibir datos del servidor: {e}")
+        
     except Exception as e:
-        print(f"Error al recibir datos del servidor: {e}")
-        break
+        return (f"Error al conectar al servidor: {e}")     
 
-client_socket.close()
+
+"""
+def conectar_servidor(server_host, server_port):
+    # Socket para la conexión TCP/IP
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        client_socket.connect((server_host, server_port))
+        print("[*] Conexión exitosa al servidor")
+    except Exception as e:
+        print(f"Error al conectar al servidor: {e}")
+        return None
+
+    while True:
+        try:
+            data = client_socket.recv(1024) 
+            if not data:
+                break
+            return ("Datos del sensor "+server_host+":" , data.decode('UTF-8'))
+        except Exception as e:
+            return (f"Error al recibir datos del servidor: {e}")
+            
+
+    client_socket.close()
 """
 
+"""
 class TCP_connection:
     def __init__(self, server_host, server_port):
         self.server_host = server_host
@@ -48,9 +67,9 @@ class TCP_connection:
                 data = self.client_socket.recv(1024)
                 if not data:
                     break
-                print("Datos del sensor " + self.server_host + ":", data.decode('UTF-8'))
+                return ("Datos del sensor " + self.server_host + ":", data.decode('UTF-8'))
             except Exception as e:
-                print(f"Error al recibir datos del servidor: {e}")
+                return (f"Error al recibir datos del servidor: {e}")
                 break
 
     def close_connection(self):
@@ -60,7 +79,8 @@ class TCP_connection:
         return "Datos del sensor " + self.server_host
 
 # Dirección y puerto del servidor
-server_host = '169.254.249.146'
+#server_host = '169.254.249.146'
+server_host = '192.168.8.109'
 server_port = 8888
 
 # Crear una instancia de la clase TCP_connection
@@ -74,6 +94,8 @@ connection.receive_data()
 
 # Cerrar conexión
 connection.close_connection()
+"""
 
-
-
+def prueba():
+    s="Temp y hum"
+    return s
